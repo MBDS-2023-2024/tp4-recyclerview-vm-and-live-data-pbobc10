a11c6f4fbec38f8c031d6b72d30cb6e78fedf0fa
pbobc10 <pbobc10@gmail.com>
2024-09-04 14:17:12 -0400

    return the list of users retrieved from the api service and add a random user

--- app/src/main/java/org/mbds/unice/github/data/repository/UserRepository.kt
+++ app/src/main/java/org/mbds/unice/github/data/repository/UserRepository.kt
@@ -1,17 +1,24 @@
 package org.mbds.unice.github.data.repository
 
 import org.mbds.unice.github.data.api.ApiService
+import org.mbds.unice.github.data.api.FakeApiServiceGenerator
+import org.mbds.unice.github.data.api.FakeApiServiceGenerator.FAKE_USERS
+import org.mbds.unice.github.data.api.FakeApiServiceGenerator.FAKE_USERS_RANDOM
 import org.mbds.unice.github.data.model.User
 
 class UserRepository(
     private val apiService: ApiService
 ) {
     fun getUsers(): List<User> {
-        TODO("Should return the list of users retrieved from the api service")
+        // Should return the list of users retrieved from the api service
+        return FAKE_USERS
     }
 
     fun addRandomUser() {
-        TODO("Should add a random user")
+        //TODO("Should add a random user")
+        val fakeUser = FAKE_USERS_RANDOM.random()
+        FAKE_USERS.add(fakeUser)
+        println("test "+ fakeUser)
     }
 
     fun deleteUser(user: User) {
